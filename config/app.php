400dab84fb33b300cf0620c92415620135ede071
DenDOme <newlifeofkirim@gmail.com>
2024-03-20 17:00:30 +0700

    done 3 | start individual | page 76

--- config/app.php
+++ config/app.php
@@ -4,6 +4,13 @@ return [
    'identity'=>\Model\User::class,
    'routeMiddleware' => [
       'auth' => \Middlewares\AuthMiddleware::class,
-      'role' => \Middlewares\RoleMiddleware::class
-   ]
+      'role' => \Middlewares\RoleMiddleware::class,
+      'trim' => \Middlewares\TrimMiddleware::class,
+      'specialChars' => \Middlewares\SpecialCharsMiddleware::class,
+      'csrf' => \Middlewares\CSRFMiddleware::class,
+   ],
+   'validators' => [
+      'required' => \Validators\RequireValidator::class,
+      'unique' => \Validators\UniqueValidator::class
+  ]
 ];
--- views/site/signup.php
+++ views/site/signup.php
@@ -1,6 +1,7 @@
 <h2 class="text-center text-4xl">Регистрация нового пользователя</h2>
 <h3><?= $message ?? ''; ?></h3>
 <form method="post" class="flex flex-col max-w-xs border-2 border-blue-700 rounded-3xl mx-auto mt-5 p-5 gap-2.5">
+   <input name="csrf_token" type="hidden" value="<?= app()->auth::generateCSRF() ?>"/>
    <label class="flex flex-col gap-1 items-center" >Имя <input class="h-10 w-60 bg-gray-200" type="text" name="name"></label>
    <label class="flex flex-col gap-1 items-center" >Логин <input class="h-10 w-60 bg-gray-200" type="text" name="login"></label>
    <label class="flex flex-col gap-1 items-center" >Пароль <input class="h-10 w-60 bg-gray-200" type="password" name="password"></label>
